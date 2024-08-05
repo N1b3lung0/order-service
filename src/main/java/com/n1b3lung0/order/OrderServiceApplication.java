df2525dfd5246742770e2c97a5a0b12f84e56e1e
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-08-05 20:07:28 +0200

    Add main

---
+++ src/main/java/com/n1b3lung0/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.n1b3lung0.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
