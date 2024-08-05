95a69afdcf36b53bd8f5ce20494e82d68b6ca0c4
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-08-05 20:06:47 +0200

    Add initial tests section

---
+++ src/test/java/com/n1b3lung0/order/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package com.n1b3lung0.order;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
