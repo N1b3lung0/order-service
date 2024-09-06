7b7842b246d1e767158f9f8e870a458135f80ecb
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-09-06 19:44:08 +0200

    Change OpenFeign by RestClient

--- src/main/java/com/n1b3lung0/order/client/InventoryClient.java
+++ src/main/java/com/n1b3lung0/order/client/InventoryClient.java
@@ -1,13 +1,10 @@
 package com.n1b3lung0.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/inventories")
+    @GetExchange("/inventories")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
