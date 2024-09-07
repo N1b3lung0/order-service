3ddbbff0218b659b0827a9e64f5b0b7a078546fa
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2024-09-07 17:51:56 +0200

    Circuit Breaker with Resilience4J

--- src/main/java/com/n1b3lung0/order/client/InventoryClient.java
+++ src/main/java/com/n1b3lung0/order/client/InventoryClient.java
@@ -1,10 +1,21 @@
 package com.n1b3lung0.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 public interface InventoryClient {
 
+    org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/inventories")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
